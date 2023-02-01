e1b31ba989865733d81ba2482a89ae0d5f369099
AntonyGN <antonygithendu@gmail.com>
2023-02-01 23:15:04 +0300

    first commit

---
+++ password_checker.py
@@ -0,0 +1,17 @@
+import re
+
+def password_strength(password):
+    if len(password) < 8:
+        return "Weak"
+    if not re.search("[a-z]", password):
+        return "Weak"
+    if not re.search("[A-Z]", password):
+        return "Weak"
+    if not re.search("[0-9]", password):
+        return "Weak"
+    if not re.search("[!@#$%^&*()_+-=<>,.?/]", password):
+        return "Weak"
+    return "Strong"
+
+password = input("Enter password: ")
+print("Password Strength:", password_strength(password))
